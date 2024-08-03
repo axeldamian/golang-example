8cf960332124208fdab846ea8be8a5e79894fb3e
axeldamian <axel.arrondo@mercadolibre.com>
2024-08-03 14:50:13 -0300

    added pie graph

--- app.go
+++ app.go
@@ -7,6 +7,13 @@ import(
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
+
+	"github.com/go-echarts/go-echarts/v2/components"
+)
+
+var (
+	itemCntPie = 4
+	seasons    = []string{"Spring", "Summer", "Autumn ", "Winter"}
 )
 
 func translado(i, j int) (int , int) {
@@ -22,11 +29,39 @@ func generateBarItems() []opts.BarData {
 	return items
 }
 
+func generatePieItems() []opts.PieData {
+	items := make([]opts.PieData, 0)
+	for i := 0; i < itemCntPie; i++ {
+		items = append(items, opts.PieData{Name: seasons[i], Value: rand.Intn(100)})
+	}
+	return items
+}
+
+func pieBase() *charts.Pie {
+	pie := charts.NewPie()
+	pie.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{Title: "basic pie example"}),
+	)
+
+	pie.AddSeries("pie", generatePieItems())
+	return pie
+}
+
 func main() {
   x , y := translado( 1 , 2 )
 	s := fmt.Sprintf("translado is %v , %v", x , y )
 	fmt.Println(s)
-	
+
+	page := components.NewPage()
+	page.AddCharts(
+		pieBase(),
+	)
+	f, err := os.Create("pie.html")
+	if err != nil {
+		panic(err)
+	}
+	//page.Render(io.MultiWriter(f))
+	page.Render(f)
 	// create a new bar instance
 		bar := charts.NewBar()
 		// set some global options like Title/Legend/ToolTip or anything else
@@ -40,6 +75,6 @@ func main() {
 			AddSeries("Category A", generateBarItems()).
 			AddSeries("Category B", generateBarItems())
 		// Where the magic happens
-		f, _ := os.Create("bar.html")
-		bar.Render(f)
+		//f, _ := os.Create("bar.html")
+		//bar.Render(f)
 }
